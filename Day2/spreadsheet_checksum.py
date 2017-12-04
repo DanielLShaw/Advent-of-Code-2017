2512828483ca8043a1670248b25bc9edae1a2731
Daniel Shaw <dls1992@live.co.uk>
2017-12-04 19:46:36 +0000

    Plan for Day 2

--- Day2/spreadsheet_checksum.py
+++ Day2/spreadsheet_checksum.py
@@ -1,4 +1,8 @@
 def execute(input_file_path):
+    # get file
+    # clean file
+    # get matrix
+    # analyse rows
     output = False
     print(input_file_path, "-->", output)
     return output
