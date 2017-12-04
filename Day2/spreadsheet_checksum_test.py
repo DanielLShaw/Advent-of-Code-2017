3293003810b5578eecc46f3c317c579895765a6d
Daniel Shaw <dls1992@live.co.uk>
2017-12-04 20:16:36 +0000

    added answer into test

--- Day2/spreadsheet_checksum_test.py
+++ Day2/spreadsheet_checksum_test.py
@@ -1,4 +1,4 @@
 import spreadsheet_checksum
 if __name__ == '__main__':
     assert spreadsheet_checksum.execute("example.tsv") == 18
-    assert spreadsheet_checksum.execute("input.tsv") == 0
+    assert spreadsheet_checksum.execute("input.tsv") == 44887
