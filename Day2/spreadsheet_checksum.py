689674188b4eac05a00452659c8dd7a5015ea04e
Daniel Shaw <dls1992@live.co.uk>
2017-12-04 20:16:50 +0000

    Day 2 answer

--- Day2/spreadsheet_checksum.py
+++ Day2/spreadsheet_checksum.py
@@ -2,17 +2,26 @@ import re
 
 
 def execute(input_file_path):
+    output = 0
     # read the input file safely
     print("opening:", input_file_path)
     with open(input_file_path, "r") as input_file:
         # get a list of rows
         rows = input_file.readlines()
         for index, row in enumerate(rows):
-            print(index, ":", row)
-            # get numbers in row with a regex (seeing as the input file has space and tabs as delimeters)
-            p = re.compile('\d')
-            numbers = p.findall(row)
 
-    output = False
+            # get numbers in row with a regex (seeing as the input file has space and tabs as delimeters)
+            p = re.compile('\d+')
+            number_strs = p.findall(row)
+            # convert list of strings to list of numbers
+            numbers = []
+            for index, number_str in enumerate(number_strs):
+                numbers.append(int(number_strs[index]))
+            print(index, ":", numbers)
+            # sort list to get the largest and smallest
+            numbers.sort()
+
+            output += numbers[len(numbers) - 1] - numbers[0]
+
     print(input_file_path, "-->", output)
     return output
