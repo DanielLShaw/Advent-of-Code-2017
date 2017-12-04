68c1171cd635509df1421f6ae90cd4429dd74c11
Daniel Shaw <dls1992@live.co.uk>
2017-12-04 20:03:35 +0000

    Got the numbers in a lsit

--- Day2/spreadsheet_checksum.py
+++ Day2/spreadsheet_checksum.py
@@ -1,8 +1,18 @@
+import re
+
+
 def execute(input_file_path):
-    # get file
-    # clean file
-    # get matrix
-    # analyse rows
+    # read the input file safely
+    print("opening:", input_file_path)
+    with open(input_file_path, "r") as input_file:
+        # get a list of rows
+        rows = input_file.readlines()
+        for index, row in enumerate(rows):
+            print(index, ":", row)
+            # get numbers in row with a regex (seeing as the input file has space and tabs as delimeters)
+            p = re.compile('\d')
+            numbers = p.findall(row)
+
     output = False
     print(input_file_path, "-->", output)
     return output
