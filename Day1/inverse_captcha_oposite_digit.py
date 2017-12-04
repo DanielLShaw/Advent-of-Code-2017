70e93dce262bf4bfeb3be89f8f79b39e2ff633f8
Daniel Shaw <dls1992@live.co.uk>
2017-12-04 19:36:00 +0000

    part 2 answer

--- Day1/inverse_captcha_oposite_digit.py
+++ Day1/inverse_captcha_oposite_digit.py
@@ -1,4 +1,26 @@
 def execute(captcha):
+    # split the captcha into a list of numbers
+    numbers = [int(d) for d in str(captcha)]
     output = 0
+    for i, number in enumerate(numbers):
+        # check the match oposite condition
+        matchOposite = match_oposite(i, numbers)
+        if matchOposite:
+            output += number
     print(captcha, "-->", output)
-    return 0
+    return output
+
+
+def match_oposite(index, numbers):
+    # find half the length of the number
+    oposite_index = int(len(numbers) / 2)
+    # find if the oposite digit is behind the number
+    if index + oposite_index >= len(numbers):
+        # find the index of the digit behind the number
+        looped_index = index + oposite_index - len(numbers)
+        if numbers[index] == numbers[looped_index]:
+            return True
+    elif numbers[index] == numbers[index + oposite_index]:
+        return True
+    else:
+        return False
