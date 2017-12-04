2f3229d6ea5d72f21441ecaad7a75e785eae1045
Daniel Shaw <dls1992@live.co.uk>
2017-12-04 19:36:08 +0000

    comments for first part

--- Day1/inverse_captcha_next_digit.py
+++ Day1/inverse_captcha_next_digit.py
@@ -1,8 +1,9 @@
 def execute(captcha):
-
+    # split the captcha into a list of numbers
     numbers = [int(d) for d in str(captcha)]
     output = 0
     for i, number in enumerate(numbers):
+        # check if the next digit matches this one
         matchAfter = match_after(i, numbers)
         if matchAfter:
             output += number
@@ -11,6 +12,7 @@ def execute(captcha):
 
 
 def match_after(index, numbers):
+    # check if this is this last number in the list
     if index == len(numbers) - 1 and numbers[0] == numbers[index]:
         return True
     elif index != len(numbers) - 1 and numbers[index + 1] == numbers[index]:
